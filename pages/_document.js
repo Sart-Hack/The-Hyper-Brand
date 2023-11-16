29c7db141939a012eb26252e751bc49e5970f32f
Pudding <sarthakgupta124@gmail.com>
2023-11-16 10:30:35 +0530

    clarity script

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,17 @@ const Document = () => {
           media="(prefers-color-scheme: dark)"
           content="#000"
         />
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "jr8hq5nbs4");
+              `,
+            }}
+          />
       </Head>
       <body>
         <Main />
